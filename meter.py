57d24379284b044e0eff3a38c1ed5cb31db7ff0d
winter <q916090796@gmail.com>
2020-02-05 00:40:22 +0800

    add LossMeter and calc_PSNR

---
+++ meter.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+import math
+
+class LossMeter(object):
+    def __init__(self):
+        super(LossMeter,self).__init__()
+        self.reset()
+
+    def reset(self):
+        self.avg=0
+        self.sum=0
+        self.count=0
+
+    def update(self,val,count):
+        self.sum+=val*n
+        self.count+=n
+        self.avg=self.sum/self.count
+
+def calc_PSNR(mse):
+    return 10*math.log10(1/mse)
+
