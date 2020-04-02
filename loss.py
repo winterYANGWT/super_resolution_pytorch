37bf119e404b066dada25de8e8566c3bed443c98
Winter <q916090796@gmail.com>
2020-04-02 12:48:15 +0800

    add some class

--- loss.py
+++ loss.py
@@ -2,6 +2,24 @@ import torch
 import torchvision
 import torch.nn as nn
 
+class BCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bce=nn.BCELoss()
+
+    def forward(self,output_image,target_images):
+        return self.bce(output_image,target_images)
+
+
+class MSELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mse=nn.MSELoss()
+
+    def forward(self,output_images,target_images):
+        return self.mse(output_images,target_images)
+
+
 class TVLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -21,38 +39,29 @@ class TVLoss(nn.Module):
         return t.size()[1]*t.size()[2]*t.size()[3]
 
 
+class FeatureLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        vgg=torchvision.models.vgg.vgg16(pretrained=True)
+        self.loss=nn.Sequential(*(list(vgg.features)[:31])).eval()
+        for param in self.loss.parameters():
+            param.requires_grad=False
+        self.mse=MESLoss()
+
+    def forward(self,real,fake):
+            return self.mse(self.loss(real),self.loss(fake))
+
+
 class SRGANLoss(nn.Module):
     def __init__(self):
-        vgg=torchvision.models.vgg.vgg16(pretrained=True)
-        loss_network=nn.Sequential(*(list(vgg.features)[:31])).eval()
-        for param in loss_network.parameters():
-            param.requires_grad=False
-        self.loss_network=loss_network
-        self.mse=nn.MSELoss()
+        self.feature_loss=FeatureLoss()
+        self.mse=MSELoss()
         self.tv=TVLoss()
 
-    def forward(self,out_labels,out_images,target_images):
-        adversarial_loss=torch.mean(1-out_labels)
-        perception_loss=self.mse(self.loss_network(out_images),self.loss_network(target_images))
-        image_loss=self.mse(out_images,target_images)
-        tv_loss=self.tv_loss(out_images)
-        return image_loss+0.001*adversarial_loss+0.006*perception_loss+tv_loss
-
-
-class BCELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.bce=nn.BCELoss()
-
-    def forward(self,output_image,target_images):
-        return self.bce(output_image,target_images)
-
-
-class MSELoss(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.mse=nn.MSELoss()
-
-    def forward(self,output_images,target_images):
-        return self.mse(output_images,target_images)
+    def forward(self,fake_labels,fake_images,real_images):
+        adversarial_loss=torch.sum(-torch.log2(1-fake_label))
+        vgg_loss=self.feature_loss(real_images,fake_images)
+        mse_loss=self.mse(fake_images,real_images)
+        tv_loss=self.tv_loss(fake_images)
+        return mse_loss+0.001*adversarial_loss+0.006*vgg_loss+tv_loss
 
